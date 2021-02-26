799fb24957a119df4084f053d253fca7fe074526
Behnam Jafari <behnamjaffarii@gmail.com>
2021-02-26 11:32:39 +0330

    Add locomotive

--- Assets/Scripts/Models/WagonHolder.cs
+++ Assets/Scripts/Models/WagonHolder.cs
@@ -6,42 +6,40 @@ using UnityEngine;
 
 public class WagonHolder : MonoBehaviour
 {
+    public float wagonDistance;
+    [SerializeField] private Wagon locomotivePrefab;
     [SerializeField] private Wagon wagonPrefab;
     [SerializeField] private List<Wagon> wagons;
 
-    public float wagonDistance;
 
     [Button]
     private void CreateWagons(int count)
     {
         RemoveWagons();
 
-        wagons = new List<Wagon> {GetComponent<Wagon>()};
+        wagons = new List<Wagon> {PrefabUtility.InstantiatePrefab(locomotivePrefab, transform) as Wagon};
         for (int i = 0; i < count; i++)
         {
-            var wagon = PrefabUtility.InstantiatePrefab(wagonPrefab) as Wagon;
+            var wagon = PrefabUtility.InstantiatePrefab(wagonPrefab, transform) as Wagon;
             wagons.Add(wagon);
         }
 
         SortWagons();
     }
 
-    [Button]
     private void RemoveWagons()
     {
-        wagons.Remove(GetComponent<Wagon>());
         for (int i = wagons.Count - 1; i >= 0; i--)
         {
             DestroyImmediate(wagons[i].gameObject);
         }
     }
 
-    [Button]
     private void SortWagons()
     {
-        for (int i = 1; i < wagons.Count; i++)
+        for (int i = 0; i < wagons.Count; i++)
         {
-            wagons[i].transform.position = transform.position + Vector3.back * (i * wagonDistance);
+            wagons[i].transform.localPosition = Vector3.back * (i * wagonDistance);
         }
     }
 
