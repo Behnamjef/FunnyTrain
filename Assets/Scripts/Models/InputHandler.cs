4fc711b8b58485aeb2893fbf18e9adf076c05884
Behnam Jafari <behnamjaffarii@gmail.com>
2021-02-26 15:41:19 +0330

    Implement input

---
+++ Assets/Scripts/Models/InputHandler.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputHandler : MonoBehaviour
+{
+    public bool isTouching;
+
+    private void Update()
+    {
+        isTouching = Input.GetMouseButton(0);
+    }
+}
--- Assets/Scripts/Models/SpeedHandler.cs
+++ Assets/Scripts/Models/SpeedHandler.cs
@@ -7,14 +7,16 @@ public class SpeedHandler : MonoBehaviour
     public float speed;
 
     [SerializeField] private float maxSpeed;
-    [Range(0, 1)] [SerializeField] private float gravityEffect;
-    [SerializeField] private float motorPower;
+    [Range(0.001f, 2)] [SerializeField] private float gravityEffect;
+    [Range(0, 2)] [SerializeField] private float motorPower;
 
     private Transform locomotive;
+    private InputHandler inputHandler;
 
     void Start()
     {
         locomotive = GetComponent<WagonHolder>().GetWagons()[0].transform;
+        inputHandler = GetComponent<InputHandler>();
     }
 
     void Update()
@@ -27,7 +29,7 @@ public class SpeedHandler : MonoBehaviour
         var value = locomotive.eulerAngles.x;
         if (value > 180)
             value -= 360;
-        speed += value * gravityEffect;
+        speed += value / (20 / gravityEffect) + (inputHandler.isTouching ? motorPower : 0);
         speed = Mathf.Clamp(speed, -maxSpeed, maxSpeed);
     }
 }
